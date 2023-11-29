a70f9138666121fe5d0642d2cec8cca8ad09d9ff
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-11-29 16:04:58 +0800

    Add files via upload

---
+++ draw squares of */draw squares of */main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  draw squares of *
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+            
+            
+        cout<<endl;
+            
+            
+            }
+}
